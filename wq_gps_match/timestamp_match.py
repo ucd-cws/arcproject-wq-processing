02260d0c2a70d2e7690f8db8d8bbef3f24530c6d
Andy Bell <bell.andy.m@gmail.com>
2016-08-18 18:32:36 -0400

    added function to read shp data as pd.df using pyshp

--- wq_gps_match/timestamp_match.py
+++ wq_gps_match/timestamp_match.py
@@ -1,5 +1,10 @@
 # match shp and water quality data by timestamp
 import pandas
+import shapefile
+import os
+import numpy as np
+from datetime import datetime
+
 
 # load a water quality file
 def wq_from_csv(csv_from_miniSonde):
@@ -19,5 +24,38 @@ def wq_from_csv(csv_from_miniSonde):
 	return wq
 
 
-# load a shapefile using the geopandas
+# load a shapefile using the pyshp
 
+
+ex = os.path.join("examples", "Arc_040413_GPS", "040413_PosnPnt.shp")
+
+
+
+
+def dateFromRecord(record):
+	"""
+	:param record: date in format of [[2013, 4, 4], '08:18:47am']
+	:return: datetime object
+	"""
+	date = '{0} {1} {2} {3}'.format(record[0][0], record[0][1], record[0][2], record[1])
+	date_object = datetime.strptime(date, '%Y %m %d %I:%M:%S%p')
+	return date_object
+
+
+# geopandas is a pain to install on windows so let's try to use pyshp to turn dbf into dataframe
+
+def shp2dataframe(fname):
+	"""
+	Makes a Pandas DataFrame from a shapefile.dbf with XY coords
+	"""
+	r = shapefile.Reader(fname)  # opens shapefile reader
+	data = []
+	for sr in r.shapeRecords():
+		dt = dateFromRecord(sr.record)
+		data.append([dt] + sr.shape.points)
+	df = pandas.DataFrame(data, columns=["Date", "XY"])
+	return df
+
+
+test = shp2dataframe(ex)
+print(test)
