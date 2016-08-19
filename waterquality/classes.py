240b7bb320366f092f58f567b563e949e700b9d6
Nick Santos <nickrsan@users.noreply.github.com>
2016-08-18 18:22:45 -0700

    Small tweaks and docstrings

--- waterquality/classes.py
+++ waterquality/classes.py
@@ -8,10 +8,19 @@ db_location = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 db_engine = None
 
 
-def connect_db():
+def connect_db(database=db_location):
+	"""
+		Just a helper function that sets up the database engine
+	:return:
+	"""
 	global db_engine
-	db_engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_location))
+	db_engine = sqlalchemy.create_engine('sqlite:///{}'.format(database))
 
 
 def db_session(engine=db_engine):
+	"""
+		provides a Session to the database - can be run for each thing that needs a new session.
+	:param engine:
+	:return:
+	"""
 	return orm.sessionmaker(bind=engine)
