b41cf49b5fcfbc90d8dc133470189a47f9d67c96
Andy Bell <bell.andy.m@gmail.com>
2016-08-19 14:54:54 -0400

    returns shp with wq data joined by timestamp

--- wq_gps_match/test_loadwq_unittest.py
+++ wq_gps_match/test_loadwq_unittest.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+from datetime import datetime
 from wq_gps_match import timestamp_match
 
 
@@ -16,6 +17,41 @@ class LoadWQ(unittest.TestCase):
 
 	def test_data_length(self):
 		self.assertEqual(timestamp_match.wq_from_csv(self.data).shape, (2128, 12))
+
+
+class LoadSHP(unittest.TestCase):
+
+	def setUp(self):
+		self.data = os.path.join("examples", "Arc_040413_GPS", "040413_PosnPnt.shp")
+		pass
+
+	def test_length(self):
+		self.assertEqual(timestamp_match.shp2dataframe(self.data).shape, (15976, 2))
+
+
+class CheckDates(unittest.TestCase):
+
+	def setUp(self):
+		self.shp = os.path.join("examples", "Arc_040413_GPS", "040413_PosnPnt.shp")
+		self.csv = os.path.join("examples", "Arc_040413_WQ", "Arc_040413_wqt_bk.csv")
+		self.shpdates = timestamp_match.shp2dataframe(self.shp)["Date"].tolist()
+		self.csvdates = timestamp_match.wq_from_csv(self.csv)["Date_Time"].tolist()
+		pass
+
+	def test_shp_ISO8601(self):
+		for dt in self.shpdates:
+			self.assertEqual(dt.strftime("%Y-%m-%dT%H:%M:%S"), dt.isoformat())
+
+	def test_csv_ISO8601(self):
+		for dt in self.csvdates:
+			self.assertEqual(dt.strftime("%Y-%m-%dT%H:%M:%S"), dt.isoformat())
+
+
+class CheckJoin(unittest.TestCase):
+
+	def setUp(self):
+		self.data = os.path.join("examples", "Arc_040413_GPS", "040413_PosnPnt.shp")
+		pass
 
 
 if __name__ == '__main__':
--- wq_gps_match/timestamp_match.py
+++ wq_gps_match/timestamp_match.py
@@ -4,7 +4,7 @@ import shapefile
 import os
 import numpy as np
 from datetime import datetime
-
+from shutil import copyfile
 
 # load a water quality file
 def wq_from_csv(csv_from_miniSonde):
@@ -12,6 +12,7 @@ def wq_from_csv(csv_from_miniSonde):
 	:param csv_from_miniSonde: raw data file containing water quality data
 	:return: water quality as pandas dataframe
 	"""
+
 	# load data from the csv starting at row 11, combine Date/Time columns using parse dates
 	wq = pandas.read_csv(csv_from_miniSonde, header=9, parse_dates=[[0, 1]])
 
@@ -21,15 +22,10 @@ def wq_from_csv(csv_from_miniSonde):
 	# drop all columns that are blank since data in csv is separated by empty columns
 	wq = wq.dropna(axis=1, how="all")
 
+	# change Date_Time to be ISO8603 (ie no slashes in date)
+	wq['Date_Time'] = pandas.to_datetime(wq['Date_Time'], format='%m/%d/%Y %I:%M:%S')
+
 	return wq
-
-
-# load a shapefile using the pyshp
-
-
-ex = os.path.join("examples", "Arc_040413_GPS", "040413_PosnPnt.shp")
-
-
 
 
 def dateFromRecord(record):
@@ -57,5 +53,137 @@ def shp2dataframe(fname):
 	return df
 
 
-test = shp2dataframe(ex)
-print(test)
+def JoinByTimeStamp(wq_df, shp_df):
+	"""
+	Joins two pandas dataframes using the Date Time fields
+	:param wq_df: water quality data frame - destination (inner join)
+	:param shp_df: dataframe from the shapefile - ie xy coordinates
+	:return: data frame with water quality data and gps corordinates
+	"""
+	# use left join to match gps data to water quality
+	mergedDF = pandas.merge(wq_df, shp_df, how="left", left_on="Date_Time", right_on="Date")
+
+	# the rows that are matches!
+	matchDF = mergedDF[mergedDF["XY"].notnull()]
+
+	# the rows with missing data
+	notmatchDF = mergedDF[mergedDF["XY"].isnull()]
+
+	return matchDF, notmatchDF
+
+
+def nrows(df):
+	# number of rows in a pandas data frame
+	n = df.shape[0]
+	return n
+
+
+def JoinMatchPercent(original, joined):
+	percent_match = float(nrows(joined)) / float(nrows(original)) * 100
+	return percent_match
+
+
+def JoinDiff(original, joined):
+	diff = nrows(original) - nrows(joined)
+	return diff
+
+
+def replaceIllegalFieldnames(df):
+	df = df.rename(columns={'CHL.1': 'CHL_VOLTS', 'DO%': 'DO_PCT'}) # TODO make this catch other potential errors
+
+	return df
+
+
+def write_shp(filename, dataframe, write_index=True):
+	"""Write dataframe w/ geometry to shapefile.
+
+	from https://github.com/ojdo/python-tools/blob/master/pandashp.py
+
+	Args:
+		filename: ESRI shapefile name to be written (without .shp extension)
+		dataframe: a pandas DataFrame with column geometry and homogenous
+				   shape types (Point, LineString, or Polygon)
+		write_index: add index as column to attribute tabel (default: true)
+
+	Returns:
+		Nothing.
+	"""
+
+	df = dataframe.copy()
+
+	geometry = df.pop('XY')
+
+	w = shapefile.Writer(shapefile.POINT)
+	for xy in geometry:
+		print(xy)
+		w.point(xy[0], xy[1])
+
+	# add fields for dbf
+	for k, column in enumerate(df.columns):
+		column = str(column)  # unicode strings freak out pyshp, so remove u'..'
+		print(column)
+		print(df.dtypes[k])
+		if np.issubdtype(df.dtypes[k], np.number): #TODO this dosn't work correctly becuase it's a py obj
+			# detect and convert integer-only columns
+			if (df[column] % 1 == 0).all():
+				df[column] = df[column].astype(np.integer)
+
+			# now create the appropriate fieldtype
+			if np.issubdtype(df.dtypes[k], np.floating):
+				w.field(column, 'N', decimal=5)
+			else:
+				w.field(column, 'I', decimal=0)
+		elif np.issubdtype(df.dtypes[k], np.object): # quick work around to force all columns in shp to be float
+			w.field(column, 'N', decimal=5)
+		else:
+			w.field(column)
+
+	# add records to dbf
+	for record in df.itertuples():
+		w.record(*record[1:])  # drop first tuple element (=index)
+
+	w.save(filename)
+
+
+def copyPRJ(in_shp, out_shp):
+	# copies source .prj to output .prj
+	# TODO might be a much better way to do this with pyproj
+	in_base = os.path.splitext(in_shp)[0]
+	src = in_base + '.prj'
+	out_base = os.path.splitext(out_shp)[0]
+	dst = out_base + '.prj'
+	copyfile(src, dst)
+
+
+def main(water_quality_csv, GPS_points, output_shapefile):
+	"""
+	:param water_quality_csv:
+	:param GPS_points:
+	:param output_shapefile:
+	:return: shapefile with water quality data matched by time stamps
+	"""
+
+	# process water quality
+	print("Processing Water Quality")
+	wq_df = wq_from_csv(water_quality_csv)
+
+	print("Processing GPS data")
+	# process gps data
+	shp_d = shp2dataframe(GPS_points)
+
+	print("Matching....")
+	# join by timestamp
+	matches = JoinByTimeStamp(wq_df, shp_d)[0]
+	matches = replaceIllegalFieldnames(matches)
+
+	print("Percent Matched: {}".format(JoinMatchPercent(wq_df, matches)))
+
+	print("Saving matches to shapefile")
+	# save matches to shapefile
+	write_shp(output_shapefile, matches)
+
+	# copy projection from source
+	copyPRJ(GPS_points, output_shapefile)
+
+
+
