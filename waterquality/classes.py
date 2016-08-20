ab865d4b30b1ce8c91f598580575d22fcc520d34
Nick Santos <nickrsan@users.noreply.github.com>
2016-08-19 17:13:04 -0700

    Additions to the ORM

--- waterquality/classes.py
+++ waterquality/classes.py
@@ -2,11 +2,16 @@ import os
 
 import sqlalchemy
 from sqlalchemy import orm
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db_name = "wqdb.sqlite"
 db_location = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), db_name)
 db_engine = None
 
+Base = declarative_base()
 
 def connect_db(database=db_location):
 	"""
@@ -24,3 +29,51 @@ def db_session(engine=db_engine):
 	:return:
 	"""
 	return orm.sessionmaker(bind=engine)
+
+water_quality_header_map = {
+	"Temp": "temp",
+	"pH": "ph",
+	"SpCond": "sp_cond",
+	"Sal": "salinity",
+	"DO%": "dissolved_oxygen_percent",
+	"DO": "dissolved_oxygen",
+	"DEP25": "dep_25",
+	"PAR": "par",
+	"RPAR": "rpar",
+	"TurbSC": "turbidity_sc",
+	"CHL": "chl",
+	# TODO: Missing other CHL
+}
+
+class WaterQualityFile(Base):
+	"""
+		This class gives us a framework to hang observations on so they can all be traced back to the same origin
+	"""
+	__tablename__ = 'water_quality_files'
+
+	id= Column(Integer, primary_key=True)
+	original_file_path = Column(String)
+	processed_file_path = Column(String)
+
+
+class WaterQuality(Base):
+	__tablename__ = 'water_quality'
+
+	id = Column(Integer, primary_key=True)
+	#site
+	water_quality_file_id = Column(Integer, ForeignKey('water_quality_files.id'))
+	file = relationship("WaterQualityFile", back_populates="records")
+	#date and time
+	#still need adjusted values
+	temp = Column(Float)
+	ph = Column(Float)
+	sp_cond = Column(Integer)
+	salinity = Column(Float)
+	dissolved_oxygen = Column(Float)
+	dissolved_oxygen_percent = Column(Float)
+	dep_25 = Column(Float)
+	par = Column(Integer)
+	rpar = Column(Integer)
+	turbidity_sc = Column(Float)
+	chl = Column(Float)
+	chl_volts = Column(Float)
