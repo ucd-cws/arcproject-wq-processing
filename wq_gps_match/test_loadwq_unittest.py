6a2f05c3dbe55b173aca2836b08474a4a4701838
Andy Bell <bell.andy.m@gmail.com>
2016-09-02 12:12:57 -0400

    added unit test for adjusting time to account for uncorrected daylight savings

--- wq_gps_match/test_loadwq_unittest.py
+++ wq_gps_match/test_loadwq_unittest.py
@@ -2,6 +2,7 @@ import unittest
 import os
 from datetime import datetime
 from wq_gps_match import timestamp_match
+import pandas
 
 
 class LoadWQ(unittest.TestCase):
@@ -40,6 +41,21 @@ class CheckDates(unittest.TestCase):
 	def test_ISO8601(self):
 		self.assertEqual(self.date_time.strftime("%Y-%m-%dT%H:%M:%S"), self.date_time.isoformat())
 
+	def test_DST(self):
+		# make example date time into a dataframe with "Date_Time"
+		self.df = pandas.DataFrame({'Date_Time': [self.date_time]})
+
+		# add/subtract an hour
+		self.plus1 = timestamp_match.dstadjustment(self.df, 1)
+		self.minus1 = timestamp_match.dstadjustment(self.df, -1)
+
+		# get first row
+		self.plus1hr = self.plus1.loc[0]['Date_Time']
+		self.minus1hr = self.minus1.loc[0]['Date_Time']
+
+		# test against string
+		self.assertEqual(self.plus1hr, datetime.strptime('2013-04-04 09:18:47', '%Y-%m-%d %H:%M:%S'))
+		self.assertEqual(self.minus1hr, datetime.strptime('2013-04-04 07:18:47', '%Y-%m-%d %H:%M:%S'))
 
 class CheckJoin(unittest.TestCase):
 
