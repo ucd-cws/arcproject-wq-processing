7f17f7ebf5137a91d3e368ef67fc182ce767b915
Andy Bell <bell.andy.m@gmail.com>
2016-12-15 17:35:04 -0500

    attempts to use Slurper to add some 2014 data

--- waterquality/load_2013_data.py
+++ waterquality/load_2013_data.py
@@ -1,17 +1,12 @@
 import os
-import glob
-from sqlalchemy import exc, orm
-from scripts import wq_gain
-from waterquality import utils
 from waterquality import classes
-from scripts import wqt_timestamp_match
+from scripts import slurp_globber
+
 
 ### DEFINE DATA PATHS ###
 base_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
 print(base_path)
 
-# import the project's toolbox
-#arcpy.ImportToolbox(os.path.join(base_path, "wq-processing-toolbox.pyt"))
 
 # path to location with data
 data = r"C:\Users\Andy\Desktop\ArcData" # or location on x drive
@@ -75,51 +70,24 @@ for vp in vert_profiles:
 session.close()
 
 
-
 #############################################################
 
-# Jan 2013
-# gain files
-gains_list = glob.glob(os.path.join(data, "Jan_2013", "*", "*", "*wqp*"))
-print(gains_list)
+##### JAN ######
 
-for wq_gain_file in gains_list:
-	try:
-		basename = os.path.basename(wq_gain_file)
-		site_id = basename.split("_")[3].upper() # site code is the forth part when split by underscores
-		gain_setting = 0 # TODO check
+jan = os.path.join(data, "Jan_2013")
 
-		# find the shapefiles to try to match
-		two_up = os.path.dirname(os.path.dirname(wq_gain_file))
+s = slurp_globber.Slurper()
+s.gain_setting = 0
+s.filename_part_site = 3
+s.slurp_gains(jan)
+s.slurp_trans(jan)
 
-		gps_pts = glob.glob(os.path.join(two_up, "*", "*ZoopChlW.shp"))[0]
-		print("{} {} {} {}".format(basename, site_id, gain_setting, gps_pts))
 
-		# run gain_wq function
-		try:
-			wq_gain.main(wq_gain_file, site_id, gain_setting, gps_pts)
-		except exc.IntegrityError as e:
-			print("Unable to import gain file. Record for this gain file "
-			                 "already exists in the vertical_profiles table.")
 
-	except:
-		print("Error")
+##### FEB ######
 
-# water quality transects
-
-transect_list = glob.glob(os.path.join(data, "Jan_2013", "Arc*", "*_WQ", "*wqt*"))
-print(transect_list)
-
-for wqt in transect_list:
-	try:
-		basename = os.path.basename(wqt)
-		site_id = basename.split("_")[3].upper()  # site code is the forth part when split by underscores
-		# find the shapefiles to try to match
-		two_up = os.path.dirname(os.path.dirname(wqt))
-		gps_pts = glob.glob(os.path.join(two_up, "*", "*PosnPnt.shp"))[0]
-		print("{} {} {}".format(wqt, gps_pts, site_id))
-		wqt_timestamp_match.main([wqt], gps_pts, site_function=site_id)
-	except exc.IntegrityError as e:
-		print("This transect is already in the database")
-	except orm.exc.NoResultFound:
-		print("The site for this file needs to be added to the database before adding data: {}".format(site_id))
+feb = os.path.join(data, "Feb_2013")
+s = slurp_globber.Slurper()
+s.filename_part_site = 3
+s.gain_setting = 0
+s.slurp_gains(feb)
