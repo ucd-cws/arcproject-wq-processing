5f113ac96923601f63daedc3ba000f72926d2932
Andy Bell <bell.andy.m@gmail.com>
2016-12-20 16:33:37 -0500

    added underscore index as site_func param

--- scripts/wqt_timestamp_match.py
+++ scripts/wqt_timestamp_match.py
@@ -465,9 +465,41 @@ def site_function_historic(*args, **kwargs):
 
 	return q  # return the session object
 
+site_function_params = {"site_part": 2}
+
+def site_function_underscore(*args, **kwargs):
+	"""
+	Site functions are passed to wq_df2database so that it can determine which site a record is from. Historic data
+	will use this function since it will parse if off the data frame as constructed in this code (which includes
+	a field for the filename, which has the site code). Future data will have another method and use a different site
+	function that will be passed to wq_df2database
+
+	:param args:
+	:param kwargs:
+	:return: site object
+	"""
 
 
-def wq_df2database(data, field_map=classes.water_quality_header_map, site_function=site_function_historic, session=None):
+
+	record = kwargs["record"]  # unpacking this way because I want it to be able to accept the kinds of args another function might receive too, since the caller will pass a bunch
+	session = kwargs["session"]  # database session from caller
+	site_part = kwargs["site_part"]
+	filename = record.get(source_field)  # get the value of the data source field (source_field defined globally)
+	try:
+		site_code = filename.split("_")[int(site_part)].upper()  # the third item in the underscored part of the name has the site code
+	except IndexError:
+		raise IndexError("Filename was unable to be split based on underscore in order to parse site name - be sure your filename format matches the site function used, or that you're using the correct site retrieval function")
+
+	try:
+		q = session.query(classes.Site).filter(classes.Site.code == site_code).one()
+	except NoResultFound:
+		raise ValueError("Skipping record with index {}. Site code [{}] not found.".format(record.get("Index"), site_code))
+
+	return q  # return the session object
+
+
+
+def wq_df2database(data, field_map=classes.water_quality_header_map, site_function=site_function_historic, site_params=site_function_params, session=None):
 	"""
 	Given a pandas data frame of water quality records, translates those records to ORM-mapped objects in the database.
 
@@ -493,7 +525,7 @@ def wq_df2database(data, field_map=classes.water_quality_header_map, site_functi
 
 		# this isn't the fastest approach in the world, but it will create objects for each data frame record in the database.
 		for row in records:  # iterates over all of the rows in the data frames the fast way
-			make_record(field_map, row[1], session, site_function,)  # row[1] is the actual data included in the row
+			make_record(field_map, row[1], session, site_function, site_params)  # row[1] is the actual data included in the row
 
 		# session.add_all(records)
 		if session_created:  # only commit if this function created the session - otherwise leave it to caller
@@ -513,7 +545,7 @@ def site_from_text(site_code, session):
 	return session.query(classes.Site).filter(classes.Site.code == site_code).one()
 
 
-def make_record(field_map, row, session, site_function):
+def make_record(field_map, row, session, site_function, site_params):
 	"""
 	 	Called for each record in the loaded and joined Pandas data frame. Given a named tuple of a row in the data frame, translates it into a waterquality object
 	:param field_map: A field map dictionary with keys based on the data frame fields and values of the corresponding database field
@@ -522,14 +554,13 @@ def make_record(field_map, row, session, site_function):
 	:param site_function: A site code or function that identifies the site and returns the site object for the record.
 	:return:
 	"""
-
 	wq = classes.WaterQuality()  # instantiates a new object
 
 	try:  # figure out whether we have a function or a text code to determine the site. If it's a text code, call site_from_text, otherwise call the function
 		if isinstance(site_function, six.string_types):
 			wq.site = site_from_text(site_code=site_function, session=session)
 		else:
-			wq.site = site_function(record=row, session=session)  # run the function to determine the record's site code
+			wq.site = site_function(record=row, session=session, **site_params)  # run the function to determine the record's site code
 	except ValueError:
 		traceback.print_exc()
 		return  # breaks out of this loop, which forces a skip of adding this object
@@ -651,7 +682,7 @@ def dst_closest_match(wq, pts):
 	return offset_df
 
 
-def main(water_quality_files, transect_gps, output_feature=None, site_function=site_function_historic, dst_adjustment=False):
+def main(water_quality_files, transect_gps, output_feature=None, site_function=site_function_historic, site_params=site_function_params, dst_adjustment=False):
 	"""
 	:param water_quality_files: list of water quality files collected during the transects
 	:param transect_gps: gps shapefile of transect tract
@@ -678,7 +709,7 @@ def main(water_quality_files, transect_gps, output_feature=None, site_function=s
 
 	print("Percent Matched: {}".format(JoinMatchPercent(wq, matches)))
 
-	wq_df2database(matches, site_function=site_function)
+	wq_df2database(matches, site_function=site_function, site_params=site_params)
 
 	if output_feature:
 		# Define a spatial reference for the output feature class by copying the input
