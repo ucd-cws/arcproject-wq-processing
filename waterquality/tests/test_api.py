0015df729449f901db9119b4ec411b90774b46fc
Nick Santos <nickrsan@users.noreply.github.com>
2017-01-11 12:13:51 -0800

    A first basic test

---
+++ waterquality/tests/test_api.py
@@ -0,0 +1,11 @@
+import unittest
+
+from waterquality import api
+import datetime
+
+import arcpy
+
+class GetWQTest(unittest.TestCase):
+	def test_data_retrieval(self):
+		d1 = datetime.datetime(2013, 4, 4)
+		api.get_wq_for_date(d1)
