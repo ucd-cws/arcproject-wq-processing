a1ba6843556097913be5041bb7b540e500921835
Nick Santos <nickrsan@users.noreply.github.com>
2017-01-20 20:08:53 -0800

    Changed number missing points
    Converted verification tests to true unit tests so we can get a list of
    passing and failing

--- scripts/verification_tests.py
+++ scripts/verification_tests.py
@@ -14,8 +14,8 @@ test_data = r"C:\Users\dsx.AD3\Box Sync\arcproject"
 class Test2013(unittest.TestCase):
 	def test_jan_2013(self):
 		s = os.path.join(test_data, r"Jan_2013\SummaryFiles\Jan2013_GPS\StatePlaneCAII\Arc_Jan2013_WQt_w_finalchl.shp")
-		verify.verify_summary_file(1, 2013, s)
+		self.assertTrue(verify.verify_summary_file(1, 2013, s))
 
 	def test_dec_2013(self):
 		s = os.path.join(test_data, r"Dec_2013\SummaryFiles\Dec2013_GPS\StatePlaneCAII\Arc_Dec2013_WQt_w_finalchl.shp")
-		verify.verify_summary_file(12, 2013, s)
+		self.assertTrue(verify.verify_summary_file(12, 2013, s))
