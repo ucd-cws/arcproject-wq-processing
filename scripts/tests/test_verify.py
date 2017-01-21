42475d8f2a0af79cd0cbeeab579da346aa0814a1
Nick Santos <nickrsan@users.noreply.github.com>
2017-01-20 19:48:44 -0800

    Getting closer - still not sure why so little of the data is matching on load

--- scripts/tests/test_verify.py
+++ scripts/tests/test_verify.py
@@ -18,24 +18,25 @@ class TestVerify(unittest.TestCase):
 								"Arc_121113_wqt_hs2", "Arc_121113_wqt_hs3", "Arc_121113_wqt_ln", "Arc_121113_wqt_ul", "Arc_121113_wqt_ul0"]
 		self.wq_12_11_path = os.path.join("testfiles", "Dec_2013", "Arc_121113", "Arc_121113_WQ")
 		self.wq_12_11 = [os.path.join(self.wq_12_11_path, filename) for filename in self.wq_12_11_files]
+		self.wq_12_11_gps = os.path.join("testfiles", "Dec_2013", "Arc_121113", "Arc_121113_GPS", "121113_PosnPnt.shp")
 
 		self.wq_12_13_files = ["Arc_121313_wqt_dv", "Arc_121313_wqt_dv_1", "Arc_121313_wqt_lc", "Arc_121313_wqt_lc_2",
 							   "Arc_121313_wqt_mz", "Arc_121313_wqt_sb", ]
 		self.wq_12_13_path = os.path.join("testfiles", "Dec_2013", "Arc_121313", "Arc_121313_WQ")
 		self.wq_12_13 = [os.path.join(self.wq_12_13_path, filename) for filename in self.wq_12_13_files]
+		self.wq_12_13_gps = os.path.join("testfiles", "Dec_2013", "Arc_121313", "Arc_121313_GPS", "121313_PosnPnt.shp")
 
-		self.gps = os.path.join("testfiles", "Dec_2013", "Arc_121113", "Arc_121113_GPS", "121113_PosnPnt.shp")
-
-	def _remake_db(self, wq_files):
+	def _remake_db(self):
 		print("Warning! Recreating Database!")
 		utils.recreate_tables()
 
 		for site in self.site_codes:
 			self._make_site(site)
 
+	def _load_data(self, wq_files, gps_data):
 		# add one set of records for Dec 2013
 		for wq_file in wq_files:
-			wqt_timestamp_match.main([wq_file], self.gps, site_func_params={"site_part": 3, "gain_part": 4})
+			wqt_timestamp_match.main([wq_file], gps_data, site_func_params={"site_part": 3, "gain_part": 4})
 
 	def _make_site(self, site):
 
@@ -55,7 +56,9 @@ class TestVerify(unittest.TestCase):
 			We should load all the data for the month into the database and then assert that it's all there.
 		:return:
 		"""
-		self._remake_db(self.wq_12_11 + self.wq_12_13)  # load all of the december data
+		self._remake_db()  # load all of the december data
+		self._load_data(self.wq_12_11, self.wq_12_11_gps)
+		self._load_data(self.wq_12_13, self.wq_12_13_gps)
 		self.assertTrue(verify.verify_summary_file(12, 2013, self.dec_2013_summary_file, max_missing_points=15))
 		# self.assertTrue(False)  # This test should be failing as constructed because not all data is loaded
 
@@ -71,7 +74,8 @@ class TestVerify(unittest.TestCase):
 			This test only loads some of the data for the month, but should then detect that records are missing
 		:return:
 		"""
-		self._remake_db(self.wq_12_11)
+		self._remake_db()
+		self._load_data(self.wq_12_11, self.wq_12_11_gps)
 		# run verification
 		self.assertFalse(verify.verify_summary_file(12, 2013, self.dec_2013_summary_file, max_missing_points=15))
 
