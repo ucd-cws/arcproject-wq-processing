69bebd5b1a325ffef9e68740173c1f315765b34a
Nick Santos <nickrsan@users.noreply.github.com>
2017-02-09 12:02:13 -0800

    installer work

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 __author__ = "ambell, nickrsan"
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
 	name='arcproject-wq',
@@ -9,8 +9,8 @@ setup(
 	license='MIT',
 	description=None,
 	long_description="",
-	install_requires=["SQLAlchemy >= 1.1.2", "six", "numpy >= 1.9.2", "pandas >= 0.16.1", "matplotlib",
-						"geodatabase_tempfile", "amaptor >= 0.1.1"],
+	requires=["SQLAlchemy >= 1.1.2", "six", "numpy >= 1.9.2", "pandas >= 0.16.1", "matplotlib",
+						"geodatabase_tempfile", "amaptor >= 0.1.1.0"],
 	author=__author__,
 	author_email="nrsantos@ucdavis.edu",
 	url='https://github.com/ucd-cws/amaptor',
