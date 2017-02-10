ec3ab9e1636959bc4609a7d005597ed420d21493
Nick Santos <nickrsan@users.noreply.github.com>
2017-02-09 16:07:16 -0800

    Code to find R executable and use it in config.

--- arcproject/scripts/config.py
+++ arcproject/scripts/config.py
@@ -3,6 +3,7 @@ Single file to collect all the configuration settings for the project.
 """
 
 import os
+from . import r_connector
 
 # path to the arcproject-wq-processing folder
 arcwqpro = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -10,6 +11,5 @@ arcwqpro = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__
 # path to reference lines for sloughs
 ref_line = os.path.join(arcwqpro, "geo", "ArcLinearReferenceRoute_Lines.shp")
 
-
 # location of r install
-rscript = r"C:\Program Files\R\R-3.2.3\bin\rscript.exe" # TODO update this depending on r install local
+rscript = r_connector.rscript
