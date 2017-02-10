ad5ddbf10f4f1c21dc367a64264442aaa2d968a3
Nick Santos <nickrsan@users.noreply.github.com>
2017-02-09 16:17:10 -0800

    Further applying "find R" code

--- arcproject/scripts/sub_gen_heat.py
+++ arcproject/scripts/sub_gen_heat.py
@@ -1,13 +1,13 @@
 import subprocess
 import os
 
-from arcproject.scripts import config
+from . import config
 
 ### DEFINE DATA PATHS ###
-base_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+base_path = config.arcwqpro
 
 # path to R exe
-rscript_path = r"C:\Program Files\R\R-3.2.3\bin\rscript.exe"
-gen_heat = os.path.join(base_path, "scripts\generate_heatplots.R")
+rscript_path = config.rscript
+gen_heat = os.path.join(base_path, "scripts", "generate_heatplots.R")
 
 subprocess.call([rscript_path, gen_heat, "--args", "CA", "salinity", "CA"])
