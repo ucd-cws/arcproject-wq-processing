1939fdbd83c301c5c2d3ec82ae01e8cca79307ef
Nick <ultraayla@gmail.com>
2019-02-26 15:56:49 -0800

    Local updates to the setup builder

--- make_distribution.ps1
+++ make_distribution.ps1
@@ -1,11 +1,11 @@
 # Clean up items
 Remove-Item .\distribution\*
 Move-Item .\dist\* .\versions -Force # save any old versions, but get them out of the dist folder so that we can copy any wheels in here to the final package
-$destination = "C:\Users\dsx.AD3\Code\arcproject-wq-processing\distribution.zip"
+$destination = "C:\Users\dsx\Code\arcproject-wq-processing\distribution.zip"
 If(Test-Path $destination) {Remove-Item $destination}
 
 # Make the wheel and copy files to the distribution folder
-& "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arproject3\python" .\setup.py bdist_wheel
+& "C:\Python27\ArcGIS10.5\python" .\setup.py bdist_wheel
 Copy-Item .\user_setup.py .\distribution
 Copy-Item .\dist\arcproject*.whl .\distribution
 Copy-Item .\*.pyt .\distribution
@@ -14,7 +14,7 @@ Copy-Item .\Installation_Instructions.url .\distribution\Installation_Instructio
 Copy-Item .\README.md .\distribution\README.txt
 
 # zip the files in the distribution folder to a single file
-$source = "C:\Users\dsx.AD3\Code\arcproject-wq-processing\distribution"
+$source = "C:\Users\dsx\Code\arcproject-wq-processing\distribution"
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::CreateFromDirectory($source, $destination)
 
